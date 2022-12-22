506b74edcee8a3a9dd4ba7eab0229c947df8868b
Nadia Magazine <77028660+nadiamagazine@users.noreply.github.com>
2022-12-22 13:12:55 -0500

    add persisting a query

--- app/src/main/java/com/nadiamagazine/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/nadiamagazine/photogallery/PhotoGalleryViewModel.kt
@@ -6,11 +6,14 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 private const val TAG = "PhotoGalleryViewModel"
 class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
+    private val preferencesRepository = PreferencesRepository.get()
+
     private val _galleryItems: MutableStateFlow<List<GalleryItem>> =
         MutableStateFlow(emptyList())
 
@@ -18,17 +21,19 @@ class PhotoGalleryViewModel : ViewModel() {
         get() = _galleryItems.asStateFlow()
     init {
         viewModelScope.launch {
-            try {
-                val items = fetchGalleryItems("planets")
-                _galleryItems.value = items
-            } catch (ex: Exception) {
-                Log.e(TAG, "Failed to fetch gallery items", ex)
+            preferencesRepository.storedQuery.collectLatest { storedQuery ->
+                try {
+                    val items = fetchGalleryItems("planets")
+                    _galleryItems.value = items
+                } catch (ex: Exception) {
+                    Log.e(TAG, "Failed to fetch gallery items", ex)
+                }
             }
         }
     }
 
     fun setQuery(query: String) {
-        viewModelScope.launch { _galleryItems.value = fetchGalleryItems(query) }
+        viewModelScope.launch { preferencesRepository.setStoredQuery(query) }
     }
 
     private suspend fun fetchGalleryItems(query: String): List<GalleryItem> {
